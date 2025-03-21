e8b149d3cc1cf3d82822d96d48aa1771cb936ba9
celiszin <teste153ovo@gmail.com>
2025-03-21 20:32:13 -0300

    1

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -14,9 +14,12 @@ namespace GStore.Controllers
     {
         private readonly AppDbContext _context;
 
-        public CategoriasController(AppDbContext context)
+        private readonly IWebHostEnvironment _host;
+
+        public CategoriasController(AppDbContext context, IWebHostEnvironment host)
         {
             _context = context;
+            _host = host;
         }
 
         // GET: Categorias
@@ -54,14 +57,29 @@ namespace GStore.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Foto")] Categoria categoria)
+        public async Task<IActionResult> Create([Bind("Id,Nome,Foto")] Categoria categoria, IFormFile Arquivo)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(categoria);
                 await _context.SaveChangesAsync();
+
+                if (Arquivo != null )
+                {
+                    string nomeArquivo = categoria.Id + Path.GetExtension(Arquivo.FileName);
+                    string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
+                    string novoArquivo = Path.Combine(caminho, nomeArquivo);
+                    using (var stream = new FileStream(novoArquivo, FileMode.Create))
+                    {
+                        Arquivo.CopyTo(stream);
+                    }
+                        categoria.Foto = "\\img\\categorias" + nomeArquivo;
+                        await _context.SaveChangesAsync();
+                }
+
                 return RedirectToAction(nameof(Index));
             }
+            TempData["Success"] = "Categoria Cadastrada Com Sucesso";
             return View(categoria);
         }
 
